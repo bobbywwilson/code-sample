49a8f27ff874d51c3e87d36b7d402673bd2e67e9
bobbywwilson <96799751+bobbywwilson@users.noreply.github.com>
2022-06-13 15:52:20 -0400

    Update redirect.php

--- customer-service-portal/custserv-2021-05-07/controllers/services/redirect.php
+++ customer-service-portal/custserv-2021-05-07/controllers/services/redirect.php
@@ -3,34 +3,34 @@
 	
 	class Redirect {
 		/**
-         * Redirect User
-         * 
-         * @param String $route
-         * @return Void
-        */
-		private function redirectUser($route) {
-			try {
-                if (! isset($route)) {
-                    exit('Please provide a url to redirct user');
-                }
+		 * Redirect User
+		 * 
+		 * @param String $route
+		 * @return Void
+		*/
+			private function redirectUser($route) {
+				try {
+			if (! isset($route)) {
+			    exit('Please provide a url to redirct user');
+			}
 
-                header('Refresh: 0;' . $route);
+			header('Refresh: 0;' . $route);
 
-                exit();
-            } catch (PDOException $exception) {	
-                echo "Unable to compete request: " . $exception->getMessage();
-                    
-                exit();
-            }
+			exit();
+		    } catch (PDOException $exception) {	
+			echo "Unable to compete request: " . $exception->getMessage();
+
+			exit();
+		    }
+			}
+
+		/**
+		 * Public Redirect User Interface
+		 * 
+		 * @param String $route
+		 * @return Void
+		*/
+		public function route($route) {
+		    return $this->redirectUser($route);
 		}
-
-        /**
-         * Public Redirect User Interface
-         * 
-         * @param String $route
-         * @return Void
-        */
-        public function route($route) {
-            return $this->redirectUser($route);
-        }
 	}
